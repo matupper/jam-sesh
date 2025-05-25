ce81cf1365c390ef58084d0bdd756d5f6f26bf4b
Matias Tupper <matupper@Matiass-MacBook-Pro.local>
2025-05-25 16:54:32 -0700

    Sync local project with Amplify backend environment

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "jamsesh1bcd60de": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
